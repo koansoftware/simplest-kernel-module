31060d9bc7e4519c45c64e4dc5d0e1d3d394e3a3
Marco Cavallini <m.cavallini@koansoftware.com>
2017-10-18 16:14:36 +0200

    hellokernel.c - Changed printk and string content by Dante

--- hellokernel.c
+++ hellokernel.c
@@ -1,7 +1,22 @@
 /*
  * hellokernel.c
  * The simplest kernel module
- * Marco Cavallini - KOAN sas - <koansoftware.com>
+ *
+ * Copyright 2017 KOAN sas - Bergamo - Italy
+ * Authored-by:  Marco Cavallini - <m.cavallini@koansoftware.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include <linux/module.h>
@@ -9,21 +24,19 @@
 
 static int __init hello_init(void)
 {
-    pr_alert("Good morrow");
-    pr_alert("to this fair assembly.\n");
+    printk("When half way through the journey of our life\n");
     return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    pr_alert("Alas, poor world, what treasure");
-    pr_alert("hast thou lost!\n");
+    printk("I found that I was in a gloomy wood\n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Greeting module");
-MODULE_AUTHOR("William Shakespeare");
+MODULE_DESCRIPTION("Greeting module from the Divine Comedy");
+MODULE_AUTHOR("Dante Alighieri");
 
